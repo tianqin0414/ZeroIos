f87de1a462d701316824818d3c79073e5d488558
ZeroIos <38795697@qq.com>
2018-05-11 02:03:22 +0800

    Initial Commit

---
+++ ZeroIos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZeroIos
+//
+//  Created by Tianq on 2018/5/11.
+//  Copyright © 2018年 zeluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZeroIos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZeroIos
+//
+//  Created by Tianq on 2018/5/11.
+//  Copyright © 2018年 zeluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
