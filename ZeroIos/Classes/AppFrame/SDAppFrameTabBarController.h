422c3ac481c873c47db3d9aae07c122f5926f8f7
ZeroIos <38795697@qq.com>
2018-05-15 14:37:10 +0800

    Tab Initial

---
+++ ZeroIos/Classes/AppFrame/SDAppFrameTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  SDAppFrameTabBarController.h
+//  ZeroIos
+//
+//  Created by Tianq on 2018/5/15.
+//  Copyright © 2018年 zeluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDAppFrameTabBarController : UITabBarController
+
+@end
