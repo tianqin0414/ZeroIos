61e4e76328aa89678c2cca6f95f189f625082338
ZeroIos <38795697@qq.com>
2018-05-15 16:19:57 +0800

    Tag Initial 1.1

--- ZeroIos/Classes/AppFrame/SDAppFrameTabBarController.m
+++ ZeroIos/Classes/AppFrame/SDAppFrameTabBarController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor blueColor];
+    //self.view.backgroundColor = [UIColor blueColor];
     // Do any additional setup after loading the view.
 }
 
